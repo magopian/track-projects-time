7230f9ca6b087a0849627bc699c7cee41805aaa9
Mathieu Agopian <mathieu@agopian.info>
2021-02-11 16:00:27 +0100

    Updates

---
+++ precache-manifest.72e15a9964c72d4eb855c74a3a87bbba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "6e4eecd0a7b3d6c2715e4eca9edcb9f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "4bd6ad822221965b03ef66ab035e00c2",
+    "url": "./shoelace.css"
+  },
+  {
+    "revision": "dee484885c66df080b3c",
+    "url": "./static/js/main.dee48488.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "./static/js/vendors~main.604c6712.chunk.js"
+  },
+  {
+    "revision": "2c51619bc74d2a4b2793de053097b635",
+    "url": "./styles.css"
+  }
+]);
